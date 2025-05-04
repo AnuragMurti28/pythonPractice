8edd9d854d3f66b53a09d898a91e864bd507c867
AnuragMurti28 <anuragmurti28@gmail.com>
2025-05-05 01:04:53 +0530

    started python basics

---
+++ firstProgram.py
@@ -0,0 +1,21 @@
+print("Anurag is my name");
+print("Apna college");
+print("My age is 30")
+print("name = 'Anurag Murti;'");
+
+name = "Anurag Murti";
+age = 30;
+price = 25.99;
+print("print(name): ",name);
+print("print(age): ",age);
+print("right = 'RIGHT' left = 'LEFT'");
+right = "RIGHT";
+left = "LEFT";
+test = "TEST";
+print("print(right): ",right);
+print("print(left): ",left);
+print("print(test): ",test);
+left = right;
+print("left = right");
+print("print(right): ",right);
+print("print(left): ",left);
